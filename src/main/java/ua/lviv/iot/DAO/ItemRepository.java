e214ade807e208843dab154489709b6f109a3024
Bohdan Shcherb <73034554+bohdanshcherba@users.noreply.github.com>
2021-05-27 21:23:04 +0300

    update readme

--- src/main/java/ua/lviv/iot/DAO/ItemRepository.java
+++ src/main/java/ua/lviv/iot/DAO/ItemRepository.java
@@ -1,10 +1,10 @@
 package ua.lviv.iot.DAO;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.lviv.iot.models.cosmeticTools.goods.MakeupBrushes;
 
 @Repository
-public interface ItemRepository extends CrudRepository<MakeupBrushes, Integer> {
+public interface ItemRepository extends JpaRepository<MakeupBrushes, Integer> {
 
 }
